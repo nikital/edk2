18ddc663f2e884d3ba348f6cbe3fa1d5c266ced4
Nikita Leshenko <nikita.leshchenko@oracle.com>
2017-07-18 18:22:05 +0300

    OvmfPkg/MptScsiDxe: Open PciIo protocol for later use
    This will give us an exclusive access to the PciIo of this device
    after it was started and until is will be stopped.
    
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=2390
    Signed-off-by: Nikita Leshenko <nikita.leshchenko@oracle.com>
    Reviewed-by: Liran Alon <liran.alon@oracle.com>

--- OvmfPkg/MptScsiDxe/MptScsi.c
+++ OvmfPkg/MptScsiDxe/MptScsi.c
@@ -35,6 +35,7 @@ typedef struct {
   UINT32                          Signature;
   EFI_EXT_SCSI_PASS_THRU_PROTOCOL PassThru;
   EFI_EXT_SCSI_PASS_THRU_MODE     PassThruMode;
+  EFI_PCI_IO_PROTOCOL             *PciIo;
 } MPT_SCSI_DEV;
 
 #define MPT_SCSI_FROM_PASS_THRU(PassThruPtr) \
@@ -295,6 +296,18 @@ MptScsiControllerStart (
 
   Dev->Signature = MPT_SCSI_DEV_SIGNATURE;
 
+  Status = gBS->OpenProtocol (
+                  ControllerHandle,
+                  &gEfiPciIoProtocolGuid,
+                  (VOID **)&Dev->PciIo,
+                  This->DriverBindingHandle,
+                  ControllerHandle,
+                  EFI_OPEN_PROTOCOL_BY_DRIVER
+                  );
+  if (EFI_ERROR (Status)) {
+    goto FreePool;
+  }
+
   //
   // Host adapter channel, doesn't exist
   //
@@ -319,10 +332,18 @@ MptScsiControllerStart (
                   &Dev->PassThru
                   );
   if (EFI_ERROR (Status)) {
-    goto FreePool;
+    goto CloseProtocol;
   }
 
   return EFI_SUCCESS;
+
+CloseProtocol:
+  gBS->CloseProtocol (
+         ControllerHandle,
+         &gEfiPciIoProtocolGuid,
+         This->DriverBindingHandle,
+         ControllerHandle
+         );
 
 FreePool:
   FreePool (Dev);
@@ -366,6 +387,13 @@ MptScsiControllerStop (
   if (EFI_ERROR (Status)) {
     return Status;
   }
+
+  gBS->CloseProtocol (
+         ControllerHandle,
+         &gEfiPciIoProtocolGuid,
+         This->DriverBindingHandle,
+         ControllerHandle
+         );
 
   FreePool (Dev);
 
