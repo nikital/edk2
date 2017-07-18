1ebfe59d0ce4b64dc9108084d086d8c7ec50e8e1
Nikita Leshenko <nikita.leshchenko@oracle.com>
2017-07-18 15:44:08 +0300

    OvmfPkg/MptScsiDxe: Probe PCI devices and look for MptScsi
    The MptScsiControllerSupported function is called on handles passed in
    by the ConnectController() boot service and if the handle is the
    lsi53c1030 controller the function would return success. A successful
    return value will attach our driver to the device.
    
    Ref: https://bugzilla.tianocore.org/show_bug.cgi?id=2390
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Nikita Leshenko <nikita.leshchenko@oracle.com>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: Aaron Young <aaron.young@oracle.com>
    Reviewed-by: Liran Alon <liran.alon@oracle.com>

---
+++ OvmfPkg/Include/IndustryStandard/FusionMptScsi.h
@@ -0,0 +1,24 @@
+/** @file
+
+    Macros and type definitions for LSI Fusion MPT SCSI devices.
+
+    Copyright (C) 2020, Oracle and/or its affiliates. All rights reserved.
+
+    This program and the accompanying materials are licensed and made available
+    under the terms and conditions of the BSD License which accompanies this
+    distribution. The full text of the license may be found at
+    http://opensource.org/licenses/bsd-license.php
+
+    THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS, WITHOUT
+    WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+//
+// Device offsets and constants
+//
+
+#define LSI_LOGIC_PCI_VENDOR_ID 0x1000
+#define LSI_53C1030_PCI_DEVICE_ID 0x0030
+#define LSI_SAS1068_PCI_DEVICE_ID 0x0054
+#define LSI_SAS1068E_PCI_DEVICE_ID 0x0058
